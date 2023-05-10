9b63bb08f89d8c81a84ed94954057ebedb4d526e
Gruzin <gruzd.roman@inbox.ru>
2023-05-10 16:39:44 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,3 +1,20 @@
 class Sun:
-    pass
-    # TODO: please add your code here
+    __instance = None
+
+    @staticmethod
+    def inst():
+        if Sun.__instance is None:
+            return Sun()
+        return Sun.__instance
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            super(Sun, cls).__new__(cls)
+        return cls.__instance
+
+
+
+p = Sun.inst()
+f = Sun.inst()
+p is f
+#True
